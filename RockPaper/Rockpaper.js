338e17b3c7a88f1638a135767934829d18b4dcac
SandersJ <sandersjack141@gmail.com>
2022-03-10 19:13:26 +0000

    updated UI

--- RockPaper/Rockpaper.js
+++ RockPaper/Rockpaper.js
@@ -12,39 +12,64 @@ function computerPlay() {
     return result;
 }
 
+
 function playRound(playerSelection, computerSelection) {
+    let pscore;
     let result;
-    let pS = playerSelection.toUpperCase();
-    let cS = computerSelection.toUpperCase();
+    let pS  = playerSelection.toUpperCase();
+    let cS  = computerSelection.toUpperCase();
 
     if (pS == "ROCK" || pS == "SCISSORS" || pS == "PAPER") {
         if (cS == "ROCK" || cS == "SCISSORS" || cS == "PAPER") {
             if (pS == cS) {
-                result = "Its a Draw, Player had: " + pS + " and Computer had: " + cS;
-            } else if (((pS == "ROCK") && (cS == "SCISSORS")) || ((pS == "SCISSORS") && (cS == "PAPER")) || ((pS == "PAPER") && (cS == "ROCK"))){
+                result = "Its a Draw, Player had: " + pS + " and Computer had: " + cS;    
+                pscore = -99;
+            } 
+            if (((pS == "ROCK") && (cS == "SCISSORS")) || ((pS == "SCISSORS") && (cS == "PAPER")) || ((pS == "PAPER") && (cS == "ROCK"))){
+                pscore = 1;
                 result = "You Win!! Player had: " + pS + " and Computer had: " + cS;
-            } else {
+            } 
+            if (((cS == "ROCK") && (pS == "SCISSORS")) || ((cS == "SCISSORS") && (pS == "PAPER")) || ((cS == "PAPER") && (pS == "ROCK"))){
                 result = "You Lost!! Player had: " + pS + " and Computer had: " + cS;
+                pscore = 0;
             }
         } else {
-            result = "Computer input: " + cS + " is not an accepted input"
+            result = "Computer input: " + cS + " is not an accepted input";
         }
     } else {
-        result = "Player input: " + pS + " is not an accepted input"
+        result = "Player input: " + pS + " is not an accepted input";
     }
 
 
+    console.log(pscore)
 
-    return result
+    return [result, pscore];
 }
 
-function game() {
-    let comp , player;
-    for (let i = 0; i < 5; i++) {
-        comp = computerPlay();
-        player = prompt("What is your input?", "ROCK, PAPER or SCISSORS")
-        console.log(playRound(player, comp));
-     }
-}
+const buttons = document.querySelectorAll('button');
+const rdiv = document.querySelector('#result')
+const dscore = document.querySelector('#score')
 
-game();
+let ptscore = 0;
+let ctscore = 0;
+let core = 0;
+
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+      
+      let play = playRound(button.id,computerPlay()); 
+      rdiv.textContent = play[0];
+      core = play[1]
+      
+      if (core == 1) {
+          ptscore++;
+      } else if (core == 0) {
+          ctscore++;
+      }
+      console.log(core)
+
+      dscore.textContent = "The Score is: " + ptscore + "-" + ctscore;
+
+    });
+  });
