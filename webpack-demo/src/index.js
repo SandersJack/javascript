b55dd73d37a1d595592e870626a98aa697707f90
JackPC <sandersjack141@gmail.com>
2022-03-18 10:24:22 +0000

    webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,14 @@
 import myName from './myName';
 import './style.css';
 import Cat from './cat.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+import printMe from './print.js';
+
 
 function component() {
     const elem = document.createElement('div');
+    const btn = document.createElement('button');
 
     elem.textContent = myName('Cody');
 
@@ -13,6 +18,14 @@ function component() {
     myCat.src = Cat;
 
     elem.appendChild(myCat);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    elem.appendChild(btn)
+
+    console.log(Data);
+    console.log(Notes);
 
     return elem;
 }
